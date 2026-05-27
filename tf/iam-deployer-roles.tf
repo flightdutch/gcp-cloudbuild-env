f054afdd45e08acb32d10a76f860ae1712a61521
flightdutch <flightdutch@gmail.com>
2026-05-27 13:33:10 +0300

    sa - add role to get files

--- tf/iam-deployer-roles.tf
+++ tf/iam-deployer-roles.tf
@@ -23,3 +23,23 @@ resource "google_project_iam_member" "deployer_bootstrap_roles" {
 
   depends_on = [google_project_service.required_apis]
 }
+
+# ==============================================================================
+# IAM ROLES FOR RUNTIME SERVICE ACCOUNT: "${var.gcp_project_id}-${var.service_account_suffix}"
+#                                        "${var.gcp_project_id}-fn-sa
+# add perms to take logs from in-car app
+# ==============================================================================
+
+# Allow service account to create objects (required for PUT Signed URLs generation)
+resource "google_storage_bucket_iam_member" "sa_storage_creator" {
+  bucket = google_storage_bucket.raw_logs.name # Auto-dependency on bucket-name
+  role   = "roles/storage.objectCreator"
+  member = "serviceAccount:${google_service_account.function_sa.email}" # Auto-dependency on SA-name
+}
+
+# Allow service account to sign tokens and URLs on behalf of itself
+resource "google_service_account_iam_member" "sa_token_creator" {
+  service_account_id = google_service_account.function_sa.name
+  role               = "roles/iam.serviceAccountTokenCreator"
+  member             = "serviceAccount:${google_service_account.function_sa.email}"
+}
